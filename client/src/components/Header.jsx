e5cf535bc0e5a2edf31f55aecb6ea39109bf0842
Nikitaverma2608 <89023534+Nikitaverma2608@users.noreply.github.com>
2024-03-19 18:10:36 +0530

    updated header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Navbar, TextInput, Button, NavbarToggle, Dropdown, Avatar, DropdownHeader} from "flowbite-react";
+import { Navbar, TextInput, Button, NavbarToggle, Dropdown, Avatar} from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch  } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
